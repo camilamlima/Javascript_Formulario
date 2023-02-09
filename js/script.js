f7b4293e2e33925b6026dce666c584ca3f5f9156
Camila Lima <camila.bruschetta@ab-inbev.com>
2023-02-08 22:16:04 -0300

    Criando mensagens de erro

--- js/script.js
+++ js/script.js
@@ -4,8 +4,48 @@ import ehMaiorDeIdade from "./validaIdade";
 const camposDoFormulario = document.querySelectorAll("[required]");
 
 camposDoFormulario.forEach((campo) => {
-  camposDoFormulario.addEventListener("blur", () => verificaCampo(campo));
+  campo.addEventListener("blur", () => verificaCampo(campo));
+  campo.addEventListener("invalid", (evento) => evento.preventDefault());
 });
+
+const tiposDeErro = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "tooShort",
+  "customError",
+];
+
+const mensagens = {
+  nome: {
+    valueMissing: "O campo de nome não pode estar vazio.",
+    patternMismatch: "Por favor, preencha um nome válido.",
+    tooShort: "Por favor, preencha um nome válido.",
+  },
+  email: {
+    valueMissing: "O campo de e-mail não pode estar vazio.",
+    typeMismatch: "Por favor, preencha um email válido.",
+    tooShort: "Por favor, preencha um e-mail válido.",
+  },
+  rg: {
+    valueMissing: "O campo de RG não pode estar vazio.",
+    patternMismatch: "Por favor, preencha um RG válido.",
+    tooShort: "O campo de RG não tem caractéres suficientes.",
+  },
+  cpf: {
+    valueMissing: "O campo de CPF não pode estar vazio.",
+    patternMismatch: "Por favor, preencha um CPF válido.",
+    customError: "O CPF digitado não existe.",
+    tooShort: "O campo de CPF não tem caractéres suficientes.",
+  },
+  aniversario: {
+    valueMissing: "O campo de data de nascimento não pode estar vazio.",
+    customError: "Você deve ser maior que 18 anos para se cadastrar.",
+  },
+  termos: {
+    valueMissing: "Você deve aceitar nossos termos antes de continuar.",
+  },
+};
 
 function verificaCampo(campo) {
   if (campo.name == "cpf" && campo.value.length >= 11) {
@@ -15,4 +55,10 @@ function verificaCampo(campo) {
   if (campo.name == "aniversario" && campo.value != "") {
     ehMaiorDeIdade(campo);
   }
+  tiposDeErro.forEach((erro) => {
+    if (campo.validity[erro]) {
+      mensagem = mensagens[campo.name][erro];
+      console.log(mensagem);
+    }
+  });
 }
